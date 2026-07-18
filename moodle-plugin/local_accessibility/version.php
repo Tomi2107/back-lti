c29fe96585f6b4073754ec129ce5c65220fbb765
Tomi07 <tom.palmeyro@gmail.com>
2026-07-18 02:50:56 -0300

    /healt de prueba

--- moodle-plugin/local_accessibility/version.php
+++
@@ -1,8 +0,0 @@
-<?php
-defined('MOODLE_INTERNAL') || die();
-
-$plugin->component = 'local_accessibility';
-$plugin->version   = 2024010100;
-$plugin->requires  = 2022041900; // Moodle 4.0+
-$plugin->maturity  = MATURITY_BETA;
-$plugin->release   = '1.0.0';
